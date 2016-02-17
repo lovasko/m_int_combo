581f7b31765fff4ebf3ea70de5ea4cb72d6c7a25
Daniel Lovasko <lovasko@freebsd.org>
2016-02-17 18:11:57 +0100

    updated example

--- examples/example.c
+++ examples/example.c
@@ -1,36 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <m_intcombo.h>
+#include <m_int_combo.h>
 
 int
 main(int argc, char** argv)
 {
 	int i;
 	int ret;
-	struct m_intcombo ic;
+	struct m_int_combo ic;
 	char* err_str;
 	intmax_t* next;
 	intmax_t lo;
 	intmax_t hi;
 
-	m_intcombo_init(&ic);
+	m_int_combo_init(&ic);
 
 	for (i = 1; i < argc; i++) {
 		sscanf(argv[i], "%lld..%lld", &lo, &hi);
-		m_intcombo_add(&ic, lo, hi);
+		m_int_combo_add(&ic, lo, hi);
 	}
 
-	m_intcombo_finalize(&ic, &next);
+	m_int_combo_finalize(&ic, &next);
 
-	while ((ret = m_intcombo_next(&ic)) == M_INTCOMBO_OK) {
+	while ((ret = m_int_combo_next(&ic)) == M_INT_COMBO_OK) {
 		for (i = 0; i < (argc-1); i++)
 			printf("%lld ", next[i]);
 		printf("\n");
 	}
 
-	m_intcombo_free(&ic);
+	m_int_combo_free(&ic);
 
-	if (ret != M_INTCOMBO_END) {
+	if (ret != M_INT_COMBO_END) {
 		m_intcombo_error_string(ret, &err_str);
 		fprintf(stderr, "ERROR: %s\n", err_str);
 		return EXIT_FAILURE;
